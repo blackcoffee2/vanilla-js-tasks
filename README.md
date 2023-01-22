11e6ada58823f9f24cac333a175b6777bb699ce0
blackcoffee2 <blackcoffee2.dev@gmail.com>
2023-01-22 15:57:22 +0500

    Implement design.

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ A tasks tracking app made in vanilla js.
 ## Branches:
 
 - master => Project home branch.
+
+## Acknowledgements,
+
+- Fonts, `https://developers.google.com/fonts`
+- Icons, `https://iconoir.com/`
--- js/index.js
+++ js/index.js
@@ -20,12 +20,29 @@ uiFromState = function () {
   tasksDiv.innerHTML = "";
   for (let i = 0; i < tasks.length; i++) {
     const task = tasks[i];
+
+    const div = document.createElement("div");
+    div.classList = "task-container";
+
     const p = document.createElement("p");
     p.innerText = task;
-    tasksDiv.appendChild(p);
+    div.appendChild(p);
+
+    const img = document.createElement("img");
+    img.src = "./static/images/bin-minus.svg";
+    img.classList = "task-img-delete";
+    div.appendChild(img);
+
+    tasksDiv.appendChild(div);
   }
 };
 
 addButton.onclick = function () {
   addTask();
+};
+
+textInput.onkeydown = (e) => {
+  if (e.key === "Enter") {
+    addTask();
+  }
 };
