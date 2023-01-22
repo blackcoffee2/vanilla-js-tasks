5d444c585c18170b83ca002aed256dad71b20f3c
blackcoffee2 <blackcoffee2.dev@gmail.com>
2023-01-22 14:05:50 +0500

    Add support for state.

--- js/index.js
+++ js/index.js
@@ -2,13 +2,27 @@ const tasksDiv = document.getElementById("tasks-div");
 const textInput = document.getElementById("text-input");
 const addButton = document.getElementById("add-button");
 
+// State
+const tasks = [];
+
+// Update State
 addTask = function () {
   const task = textInput.value;
   if (task) {
+    tasks.push(task);
+    textInput.value = "";
+    uiFromState();
+  }
+};
+
+// Build UI from state
+uiFromState = function () {
+  tasksDiv.innerHTML = "";
+  for (let i = 0; i < tasks.length; i++) {
+    const task = tasks[i];
     const p = document.createElement("p");
     p.innerText = task;
     tasksDiv.appendChild(p);
-    textInput.value = "";
   }
 };
 
